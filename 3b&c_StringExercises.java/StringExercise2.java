99a12ec3107fedb0715dcf56f09417a74fa96233
Mehek04 <153306587+Mehek04@users.noreply.github.com>
2025-05-20 09:50:22 -0300

    StringExercise2.java

--- 3b&c_StringExercises.java/StringExercise2.java
+++ 3b&c_StringExercises.java/StringExercise2.java
@@ -1,3 +1,5 @@
+/*3B2. Write a Java Program for Counting how many times a substring appears in a main string
+using user defined function countOccurrences()*/
 package assignment;
 import java.util.Scanner;
 public class SubstringCount {
