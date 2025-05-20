0616553540fd05ee0d2e86e52f4b77c916acd6a3
Mehek04 <153306587+Mehek04@users.noreply.github.com>
2025-05-20 09:59:22 -0300

    StringExercise7.java

--- 3b&c_StringExercises.java/StringExercise7.java
+++ 3b&c_StringExercises.java/StringExercise7.java
@@ -1,3 +1,5 @@
+/* 7.Write a Java Program for Shortening a string to a specified length and adds an ellipsis using user
+defined function truncate()*/
 package assignment;
 import java.util.Scanner;
 public class StringTruncation {
