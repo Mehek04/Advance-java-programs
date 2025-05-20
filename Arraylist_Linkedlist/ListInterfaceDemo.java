4aaf4f0dc6dcfb1fa50338e5569cbbb9533abd9f
Mehek04 <153306587+Mehek04@users.noreply.github.com>
2025-05-19 23:41:36 -0300

    ListInterfaceDemo.java

--- Arraylist_Linkedlist/ListInterfaceDemo.java
+++ Arraylist_Linkedlist/ListInterfaceDemo.java
@@ -1,3 +1,8 @@
+/*1.a Develop a Java program for adding elements [Apple, Banana, Orange] into an ArrayList
+and a LinkedList to perform the following operations with different functions directed as
+follows
+1. Adding elements, 2. Adding element at specific index, 3. Adding multiple elements, 4.
+Accessing elements,5. Updating elements, 6. Removing elements, 7. Searching elements, 8. List*/
 package assignment;
 import java.util.*;
 public class ListInterfaceDemo {
